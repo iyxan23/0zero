7583e4ce1a41da5fac0f8a2f38249eac524b3130
GianXDDDDD <borcillofg2020@gmail.com>
2021-08-19 23:13:54 +0800

    refactor: Convert Java classes to Kotlin

--- android/src/com/iyxan23/gianxddddd/zerozero/AndroidLauncher.kt
+++ android/src/com/iyxan23/gianxddddd/zerozero/AndroidLauncher.kt
@@ -1,17 +1,15 @@
-package com.iyxan23.gianxddddd.zerozero;
+package com.iyxan23.gianxddddd.zerozero
 
-import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication
+import android.os.Bundle
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
+import com.iyxan23.gianxddddd.zerozero.Zero
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.iyxan23.gianxddddd.zerozero.Zero;
-
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Initialize game & config
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new Zero(), config);
-	}
+class AndroidLauncher : AndroidApplication() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize game & config
+        val config = AndroidApplicationConfiguration()
+        initialize(Zero(), config)
+    }
 }
--- desktop/src/com/iyxan23/gianxddddd/zerozero/desktop/DesktopLauncher.kt
+++ desktop/src/com/iyxan23/gianxddddd/zerozero/desktop/DesktopLauncher.kt
@@ -1,18 +1,20 @@
-package com.iyxan23.gianxddddd.zerozero.desktop;
+package com.iyxan23.gianxddddd.zerozero.desktop
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.iyxan23.gianxddddd.zerozero.Zero;
+import kotlin.jvm.JvmStatic
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.iyxan23.gianxddddd.zerozero.Zero
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		// Initialize game config & set values
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "0zero";
-		config.width = 1280;
-		config.height = 720;
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        // Initialize game config & set values
+        val config = LwjglApplicationConfiguration()
+        config.title = "0zero"
+        config.width = 1280
+        config.height = 720
 
-		// Initialize game
-		new LwjglApplication(new Zero(), config);
-	}
+        // Initialize game
+        LwjglApplication(Zero(), config)
+    }
 }
