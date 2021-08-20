dbcb95d45a001124c6eed965dc57def53c7bd251
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-20 13:56:08 +0700

    chore: make inits lazy on ApplicationAdapter

--- core/src/com/iyxan23/gianxddddd/zerozero/Zero.kt
+++ core/src/com/iyxan23/gianxddddd/zerozero/Zero.kt
@@ -6,22 +6,20 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.utils.ScreenUtils
 
 class Zero : ApplicationAdapter() {
-    var batch: SpriteBatch? = null
-    var img: Texture? = null
-    override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
-    }
+    private val batch: SpriteBatch by lazy { SpriteBatch() }
+    private val img: Texture by lazy { Texture("badlogic.jpg") }
+
+    override fun create() {}
 
     override fun render() {
         ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
-        batch!!.end()
+        batch.begin()
+        batch.draw(img, 0f, 0f)
+        batch.end()
     }
 
     override fun dispose() {
-        batch!!.dispose()
-        img!!.dispose()
+        batch.dispose()
+        img.dispose()
     }
 }
