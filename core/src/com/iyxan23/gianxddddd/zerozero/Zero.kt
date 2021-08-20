6a77c595cdd04a332bd34ec9ba666c7ea73000ff
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-20 13:58:05 +0700

    chore: make inits lazy on Game

--- core/src/com/iyxan23/gianxddddd/zerozero/Zero.kt
+++ core/src/com/iyxan23/gianxddddd/zerozero/Zero.kt
@@ -5,39 +5,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.iyxan23.gianxddddd.zerozero.data.Preferences
 import com.iyxan23.gianxddddd.zerozero.screen.SplashScreen
 
-<<<<<<< HEAD
-class Zero : ApplicationAdapter() {
-    private val batch: SpriteBatch by lazy { SpriteBatch() }
-    private val img: Texture by lazy { Texture("badlogic.jpg") }
-
-    override fun create() {}
-
-    override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
-
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
 class Zero : Game() {
-    var batch: SpriteBatch? = null
-    var preferences: Preferences? = null
+    private val batch: SpriteBatch by lazy { SpriteBatch() }
+    private val preferences: Preferences by lazy { Preferences() }
 
     override fun create() {
-        // Initialize batch and preferences
-        preferences = Preferences()
-        batch = SpriteBatch()
-
-
 
         setScreen(SplashScreen(this))
     }
 
     override fun dispose() {
         // Resources are no longer used, time to dispose
-        batch!!.dispose()
+        batch.dispose()
     }
 }
